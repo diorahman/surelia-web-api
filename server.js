7721515e7b636761df8fe84ff4280eae7079fab7
Dhi Aurrahman <diorahman@rockybars.com>
2014-05-22 10:17:19 +0700

    Changes: make it run

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+var koa = require("koa");
+var mount = require("koa-mount");
 var options = {
   root : __dirname + "/endpoints",
   db : "mongodb://localhost/test-api",
   driver : require ("mongoose")
 }
 
-koa()(require ("./")(options).listen(3000);
-console.log ("running, 3000");
+var api = require ("./")(options);
+var server = koa();
+server.use(mount(api));
+server.listen(3001);
+
+console.log ("3001");
